04a5feb80dbe546fbfe4b28788aa515fe442ee44
xuweitao <xuweitao.py@hotmail.com>
2019-09-20 16:41:02 +0800

    add features

--- model.py
+++ model.py
@@ -12,6 +12,7 @@ import time
 from sklearn import preprocessing
 from sklearn.preprocessing import PolynomialFeatures
 
+from features import cal_features, cal_pcrr
 
 print (tf.__version__)
 
@@ -22,44 +23,38 @@ print('Found GPU at: {}'.format(device_name))
 
 
 # path to the training data
-os.chdir("./train_set")
+#os.chdir("./train_set")
 
 
-    
 
 
-inst, label = [], []
-test_X, test_y = [], []
+with open("data1.csv") as file:
+    my_data = pd.read_csv(file).to_numpy()
+    #my_data = genfromtxt(file, delimiter=',')[1:]
+print (my_data.shape)
+print (type(my_data))
 
-file_id = 0
+train_X = np.array(my_data[:-10000, :-1])
+train_y = np.array(my_data[:-10000, -1])
+test_X = np.array(my_data[-10000:, :-1])
+test_y = np.array(my_data[-10000:, -1])
+test_y = test_y.reshape(-1, 1)
 
-for file in glob.glob("*.csv"):
-    
-    my_data = genfromtxt(file, delimiter=',')[1:]
-    if file_id < 5:
-        test_X = test_X + list(my_data[:, :-1])
-        test_y = test_y + list(my_data[:, -1])
-    else:
-        inst = inst + list(my_data[:, :-1])
-        label = label + list(my_data[:, -1])
-    
-    file_id += 1
-    print(file)
+for f in cal_features(train_X):
+    train_X = np.column_stack((train_X, f))
 
-train_X = np.array(inst)
-train_y = np.array(label)
-test_X = np.array(test_X)
-test_y = np.array(test_y)
-
+for f in cal_features(test_X):
+    test_X = np.column_stack((test_X, f))
 
 print (train_X.shape)
 
-poly = PolynomialFeatures(2)
-train_X = poly.fit_transform(train_X)
+#poly = PolynomialFeatures(2)
+# train_X = poly.fit_transform(train_X)
 
 #min_max_scaler = preprocessing.MinMaxScaler()
 #train_X = min_max_scaler.fit_transform(train_X)
 train_X = preprocessing.scale(train_X)
+test_X = preprocessing.scale(test_X)
 
 print (train_y.shape, train_X.shape, test_X.shape, test_y.shape)
 
@@ -75,16 +70,15 @@ class Batch(object):
     return self.X[indices], self.y[indices]
 
 
-batch_size = 2056
+batch_size = 2046 * 4
 batch = Batch(train_X, train_y, batch_size)
 
-input_size = train_X.shape[0]
 
-# Initialize placeholders 
-x = tf.placeholder(dtype = tf.float32, shape = [None, 171], name="myInput")
+# Initialize placeholders
+x = tf.placeholder(dtype = tf.float32, shape = [None, 20], name="myInput")
 y = tf.placeholder(dtype = tf.int32, shape = [None, 1])
 
-# Fully connected layer 
+# Fully connected layer
 dense = tf.contrib.layers.fully_connected(x, 128, tf.nn.relu)
 dropout = tf.layers.dropout(
     inputs=dense, rate=0.5, training=True)
@@ -108,7 +102,7 @@ dropout = tf.layers.dropout(
 dense = tf.contrib.layers.fully_connected(dropout, 256, tf.nn.relu)
 dropout = tf.layers.dropout(
     inputs=dense, rate=0.5, training=True)
-    
+
 dense = tf.contrib.layers.fully_connected(dropout, 128, tf.nn.relu)
 dropout = tf.layers.dropout(
     inputs=dense, rate=0.5, training=True)
@@ -119,7 +113,7 @@ logits = tf.identity(logits, name="myOutput")
 # Define a loss function
 loss = tf.losses.mean_squared_error(labels = y, predictions = logits)
 #loss = tf.reduce_mean(tf.square(logits - y))
-# Define an optimizer 
+# Define an optimizer
 train_op = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 
 # Define an accuracy metric
@@ -135,7 +129,7 @@ print (type(train_X), train_X.shape)
   # Add a scalar summary for the snapshot loss.
 tf.summary.scalar('loss', loss)
 
-for i in range(30000):
+for i in range(10000):
         #print('EPOCH', i)
         batch_x, batch_y = batch.getBatch()
         batch_y = batch_y.reshape(-1, 1)
@@ -143,7 +137,9 @@ for i in range(30000):
         #print (batch_x, batch_y)
         _, loss_val = sess.run([train_op, loss], feed_dict={x: batch_x, y: batch_y})
         if i % 100 == 0:
-            print(f"#{i} iteration, Loss: {loss_val}")
+            validation = sess.run([loss], feed_dict={x:test_X, y: test_y})
+            pcrr = cal_pcrr(test_y, validation)
+            print(f"#{i} iteration, Loss: {loss_val}, Validation: {validation}, pcrr: {pcrr}")
         #print('DONE WITH EPOCH')
 
 #_logits = sess.run([logits], feed_dict={x: test_X})
